6e4f15dab8947f53e15121ad9ed400a082cc5a98
bernieyeater <50559129+bernieyeater@users.noreply.github.com>
2022-09-13 09:16:01 -0400

    Commit
    commit

---
+++ src/package1/D20220913_Num_to_String.java
@@ -0,0 +1,21 @@
+package package1;
+
+public class D20220913_Num_to_String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = -187;
+		System.out.println(Integer.toString(num));
+	}
+
+}
+/*
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+*/
