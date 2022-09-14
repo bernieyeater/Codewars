bdef34a02d22e55664e0a91557d6e193969b31dc
bernieyeater <50559129+bernieyeater@users.noreply.github.com>
2022-09-14 17:30:54 -0400

    Commit
    Commit

--- src/package1/D20220914_PrinterError.java
+++ src/package1/D20220914_PrinterError.java
@@ -4,11 +4,18 @@ public class D20220914_PrinterError {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s="0123456";
+		String s="aaaxbbbbyyhwawiwjjjwwm";
 		int lengthOfString=s.length();
-		int errorsPrinting;
+		int errorsPrinting=0;
 		String [] sSplit = s.split("",0);
-		System.out.println(lengthOfString+" "+ sSplit[0]);
+		System.out.println(lengthOfString+" "+ sSplit[1]);
+		
+		for (String var : sSplit) 
+		{ 
+		    if (var.charAt(0) >  'm' ) {errorsPrinting++;}
+		}
+		String outString = errorsPrinting+"/"+lengthOfString;
+		System.out.println(outString);
 				
 	}
 
@@ -35,6 +42,19 @@ s="aaabbbbhaijjjm"
 printer_error(s) => "0/14"
 
 s="aaaxbbbbyyhwawiwjjjwwm"
+
 printer_error(s) => "8/22"
+
+Best Practice:
+
+
+
+public class Printer {
+    
+    public static String printerError(String s) {
+        return s.replaceAll("[a-m]", "").length() + "/" + s.length();
+    }
+}
+
 
 */
