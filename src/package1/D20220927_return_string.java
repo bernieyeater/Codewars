a4e58b696e099220f996d7588ff354c2ae276466
bernieyeater <50559129+bernieyeater@users.noreply.github.com>
2022-09-29 15:48:55 -0400

    Commit
    Commit

--- src/package1/D20220927_return_string.java
+++ src/package1/D20220927_return_string.java
@@ -5,6 +5,22 @@ public class D20220927_return_string {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	    if (b) {return "Yes";} else { return "No";}
 	}
 
 }
+
+/*
+
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+Others
+{
+  public static String boolToWord(boolean b)
+  {
+    return b ? "Yes" : "No";
+  }
+  
+}
+ */
+
