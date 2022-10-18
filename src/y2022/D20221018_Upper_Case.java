59141ded3d37c67107fb576c6444a9c7dcca682e
bernieyeater <50559129+bernieyeater@users.noreply.github.com>
2022-10-18 14:19:12 -0400

    Commit
    Commit

---
+++ src/y2022/D20221018_Upper_Case.java
@@ -0,0 +1,22 @@
+package y2022;
+
+public class D20221018_Upper_Case {
+
+	public D20221018_Upper_Case() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="hello";
+		String outString = str.toUpperCase();
+		System.out.println(outString);
+		
+		
+
+	}
+
+}
+/*
+ * Write a function which converts the input string to uppercase.
+*/
