0cb6efae0ba2967e9190feba1157c5151b00daf0
bernieyeater <50559129+bernieyeater@users.noreply.github.com>
2022-10-20 15:35:04 -0400

    Commit

---
+++ src/learning/Sandwich.java
@@ -0,0 +1,49 @@
+package learning;
+
+import java.util.ArrayList;
+
+public class Sandwich {
+	String breadType;
+	double baseCost;
+	double costPerItem;
+	ArrayList<String> item;
+	
+	public Sandwich(String breadType, double baseCost, double costPerItem, ArrayList<String> item) {
+		this.breadType = breadType;
+		this.baseCost = baseCost;
+		this.costPerItem = costPerItem;
+		this.item = item;
+	}
+	
+	public String getBreadType() {
+		return breadType;
+	}
+
+	public double getBaseCost() {
+		return baseCost;
+	}
+
+	public double getCostPerItem() {
+		return costPerItem;
+	}
+
+	public ArrayList<String> getItem() {
+		return item;
+	}
+
+	}
+
+	
+
+	
+	
+		
+
+
+
+
+
+	//public Sandwich() {
+		
+	//}
+}
